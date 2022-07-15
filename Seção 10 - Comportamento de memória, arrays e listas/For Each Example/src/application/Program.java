62a0ad5686df175337b8dbd5128764fdc8af573d
salviato <thiago_salviato@outlook.com>
2022-07-15 00:03:21 -0300

    add alteracoes secao 10

--- Seção 10 - Comportamento de memória, arrays e listas/For Each Example/src/application/Program.java
+++ Seção 10 - Comportamento de memória, arrays e listas/For Each Example/src/application/Program.java
@@ -1,12 +1,8 @@
 package application;
- 
+
 public class Program {
     public static void main(String[] args) {
         
-        int x = 20;
-
-        Object obj = x;
-
-        int y = (int) obj;
-    }
+        String[] vect = new String[]
+    }    
 }
