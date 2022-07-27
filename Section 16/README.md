35dcf7cd2c9ad3dac830519d13f5abdcb3a6f532
thiagosalviato <thiago_salviato@outlook.com>
2022-07-27 01:05:28 -0300

    changes in section 16 and add section 17

---
+++ Section 16/README.md
@@ -0,0 +1,39 @@
+# Java Console Chess :chess_pawn:
+A console chess game developed to practice the object orientation paradigm.
+
+![](https://raw.githubusercontent.com/roanrobersson/assets/master/java-console_chess/demo.gif)
+
+## Requeriments 🔧
+- Java version 8 or higher.
+
+## How to execute
+
+#### # Clone the repository using a **[Git Bash](https://gitforwindows.org/)** (Git Bash can show colors)
+```bash 
+git clone https://github.com/thiagosalviato/chess.git 
+```
+
+#### # Import the project in **[Intellij IDEA Ultimate](https://www.jetbrains.com/pt-br/idea/)** or any other Java IDE
+
+#### # Build the project
+
+#### # Again on **[Git Bash](https://gitforwindows.org/)**, go to executable folder
+```bash
+cd chess/bin/
+```
+
+#### # Run the game
+```bash
+java application/Program
+```
+
+## Information :memo:
+- For move a piece, type your position to select it, then type the destination of the selected piece in the format `line` +`column`, eg: `e1`
+
+#### Pieces symbols
+| Pawn | Rook |  Knight | Bishop |  Queen | King |
+|---|---|---|---|---|---|
+| **P** | **R** | **N** | **B** | **Q** | **K** |
+
+## Class diagram
+![](https://raw.githubusercontent.com/roanrobersson/assets/master/java-console_chess/class-diagram.png)
---
+++ Section 16/src/chess/pieces/King.java
@@ -0,0 +1,111 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.Color;
+
+public class King extends ChessPiece {
+
+    private ChessMatch chessMatch;
+    public King(Board board, Color color, ChessMatch chessMatch) {
+        super(board, color);
+        this.chessMatch = chessMatch;
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+
+    private boolean canMove(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCastling(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        // above verification
+        p.setValues(position.getRow() - 1, position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // below verification
+        p.setValues(position.getRow() + 1, position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // left verification
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // right verification
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // northwest verification
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // northeast verification
+        p.setValues(position.getRow() - 1, position.getColumn() + 1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // south-west verification
+        p.setValues(position.getRow() + 1, position.getColumn() - 1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // southeast verification
+        p.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // castling verification
+        if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+            // castling kingside rook
+            Position posT1 = new Position(position.getRow(),position.getColumn() + 3);
+            if(testRookCastling(posT1)){
+                Position p1 = new Position(position.getRow(),position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(),position.getColumn() + 2);
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null){
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+            // castling queenside rook
+            Position posT2 = new Position(position.getRow(),position.getColumn() - 4);
+            if(testRookCastling(posT2)){
+                Position p1 = new Position(position.getRow(),position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(),position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(),position.getColumn() - 3);
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
+        }
+
+        return mat;
+    }
+}
