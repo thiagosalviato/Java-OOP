beb802a3d45c9d1a2508c977594fd9ad7e5fae82
thiagosalviato <thiago_salviato@outlook.com>
2022-08-10 01:37:12 -0300

    update section 22

---
+++ Section 22/JPA/src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import Domain.Person;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person(1, "Thiago Salviato", "thiago@gmail.com");
+        System.out.println(p1);
+    }
+}
